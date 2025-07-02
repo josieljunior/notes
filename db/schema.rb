3c544304165e273475a7c9a1bf3b8bd84e67f889
josieljunior <josiel-junior2@hotmail.com>
2025-07-02 22:42:33 +0000

    feat: create model memo

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_07_02_222405) do
+ActiveRecord::Schema[8.0].define(version: 2025_07_02_223525) do
+  create_table "memos", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "notes", force: :cascade do |t|
     t.string "title"
     t.text "content"
