88afca37ab197901d04b4ebc64350f53bdd7dad8
josieljunior <josiel-junior2@hotmail.com>
2025-07-02 22:50:34 +0000

    feat: implement MemosController and set root route to memos#index

---
+++ app/controllers/memos_controller.rb
@@ -0,0 +1,26 @@
+class MemosController < ApplicationController
+  def index
+    @memos = Memo.all
+  end
+
+  def create
+    @memo = Memo.new(memo_params)
+    if @memo.save
+      redirect_to memos_path, notice: "Nota criada com sucesso."
+    else
+      render :new
+    end
+  end
+
+  def new
+    @memo = Memo.new
+  end
+
+  def memo_params
+    params.require(:memo).permit(:title, :content)
+  end
+
+  def show
+    @memo = Memo.find(params[:id])
+  end
+end
